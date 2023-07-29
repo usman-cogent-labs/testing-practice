b29158b5914716b73180351d2326d2a8db091adc
usman-cogent-labs <109211422+usman-cogent-labs@users.noreply.github.com>
2023-07-29 16:25:42 +0500

    Tests grouped

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,15 +1,16 @@
 import {render, screen} from '@testing-library/react'
 import Greet from './greet'
 
-
-test('Greet renders perfectly', () => {
-   render(<Greet/>)
-   const textElement = screen.getByText(/greetings/i);
-   expect(textElement).toBeInTheDocument();
-})
-
-test('Greet by name', () => {
-    render(<Greet name="Usman"/>)
-    const textElement = screen.getByText('Greetings Usman');
-    expect(textElement).toBeInTheDocument();
+describe('Greet', () => {
+    test('renders perfectly', () => {
+       render(<Greet/>)
+       const textElement = screen.getByText(/greetings/i);
+       expect(textElement).toBeInTheDocument();
+    })
+    
+    test('renders a name', () => {
+        render(<Greet name="Usman"/>)
+        const textElement = screen.getByText('Greetings Usman');
+        expect(textElement).toBeInTheDocument();
+    })
 })
